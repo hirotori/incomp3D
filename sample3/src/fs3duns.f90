2632fa33bc87a02f080bfee696debaf378c121eb
hirot <hirotori620@gmail.com>
2022-09-26 10:27:09 +0900

    sample3 updated
    sample3にコマンドラインオプション追加.計算結果の簡単な説明.

--- sample3/src/fs3duns.f90
+++ sample3/src/fs3duns.f90
@@ -1,19 +1,37 @@
-program fs3duns
+program fs3duns_pois
     !/////////////////////////////////////////////////////////////////
     !! version: 1.0.0
     !! author: T.Ikeda
     !! summary:
     !! - メイン関数. 
-    !! - あらかじめ用意されたケースクラス, シミュレータ, アルゴリズムにより
-    !!   解析を行うサンプルプログラム. 
+    !! - ポアズイユ流のサンプル.
     !/////////////////////////////////////////////////////////////////
+    use command_line_args_m
     use case_poiseuille_m
+    use fractional_step_m
     use fractional_step_implicit_m
     use simulator_m
-
+    type(args_t),allocatable :: args(:)
     type(case_poiseuille_t) :: current_case
     type(simulator_t) :: simulator
-    type(solver_fs_imp_t) :: solver
+    class(solver_fs),allocatable :: solver
+
+    args = argv()
+    if ( size(args) /= 2 ) then
+        print "('USAGE :: ')"
+        print "('./fs3duns_pois.exe [solver type]')"
+        print "('[solver type] = ""explicit"" or ""implicit""')"
+        error stop
+    end if
+
+    if ( args(2)%v == "explicit" ) then
+        allocate(solver_fs :: solver)
+    else if ( args(2)%v == "implicit" ) then
+        allocate(solver_fs_imp_t :: solver)
+    else 
+        error stop "invalid argument :: "//args(2)%v
+    end if
+
     call simulator%run(current_case, solver)
    
-end program fs3duns
+end program fs3duns_pois
